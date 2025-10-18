2cc969c441e1eaa8b29a17c8560c3653775075c4
inetke <ineta.keryte@gmail.com>
2025-10-18 17:00:31 +0000

    added the last changes

--- src/app.py
+++ src/app.py
@@ -3,8 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 
-# load the .env file variables
 load_dotenv()
 
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
 
-# Spotify API credentials
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+spotify = spotipy.Spotify(auth_manager=auth_manager)
